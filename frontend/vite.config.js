fff977e62a13a79c8cdca2f299e6ea5ee87fc393
yehanj65 <yehanj65@gmail.com>
2026-06-02 10:00:34 +0530

    Frontend changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://salon-booking-saas-api.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  },
+});
